00f7e6a44f2356925eae57382c847c015b3d033f
Jason Gifford <jason.gifford@nielsen.com>
2021-12-09 13:25:12 -0500

    Adding javascript directory

---
+++ javascript/filter.js
@@ -0,0 +1,24 @@
+#! /usr/bin/js
+//Filter is boolean by nature
+const obj = [
+	{name : "Jason", premium: true},
+	{name : "William", premium: true},
+	{name : "Other Shmuck", premium: false}
+	]
+let results = obj.filter(item => {
+	return item.premium == true
+	}) //returns objects that have premium true
+console.log(results)
+
+// shorthand
+results = obj.filter(item => item.premium) //same fucking thing!
+console.log(results)
+//[
+//  { name: 'Jason', premium: true },
+//  { name: 'William', premium: true }
+//]
+//[
+//  { name: 'Jason', premium: true },
+//  { name: 'William', premium: true }
+//]
+//
---
+++ javascript/map.js
@@ -0,0 +1,40 @@
+#! /usr/bin/js
+//Map performs an operation on an array and returns the modified array
+const prices = [20, 10, 5, 1201, 12901]
+
+let halfOffPrices = prices.map((price) => {
+	return price / 2
+})
+console.log(halfOffPrices)
+
+//Shorthand
+halfOffPrices = prices.map(price => price / 2)
+console.log(halfOffPrices)
+ 
+// [ 10, 5, 2.5, 600.5, 6450.5 ]
+// [ 10, 5, 2.5, 600.5, 6450.5 ]
+
+//This can be done with other objects as well
+const obj = [
+	{name: 'Jason', price: 5},
+	{name: 'Jackie', price: 5000000},
+	{name: 'Will', price: 9999999999}
+]
+
+//this unlike the others above cannot be
+//shortened into one line
+const newob = obj.map(person => {
+	return {name: person.name, price: person.price / 2}
+	/*
+		product.price = product.price / 2
+	  would alter the actual array
+	*/
+	})
+console.log(newob)
+
+//[
+//  { name: 'Jason', price: 2.5 },
+//  { name: 'Jackie', price: 2500000 },
+//  { name: 'Will', price: 4999999999.5 }
+//]
+
