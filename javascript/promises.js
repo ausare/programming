bbc6885e034255c973cd7efc32a95a4edf1578bd
Jason Gifford <jason.gifford@nielsen.com>
2021-12-30 09:15:07 -0500

    Changes and renamed file

--- javascript/promises.js
+++ javascript/promises.js
@@ -1,1 +1,10 @@
-#!/usr/local/bin/node
+//promise in it's simplest form
+const getSomething = () => {
+	// resolve, reject are built into promises
+	return new Promise((resolve, reject) => {
+		// fetch something
+		resolve('yo, this was resolved')
+		reject('hey man, what did you do?')
+	});
+}
+
