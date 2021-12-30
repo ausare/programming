d12a663b0600fc48c7b99a573955f28f831695df
Jason Gifford <jason.gifford@nielsen.com>
2021-12-30 11:30:29 -0500

    Changes promises.js

--- javascript/promises.js
+++ javascript/promises.js
@@ -1,10 +1,18 @@
 //promise in it's simplest form
+//you're having trouble understanding, kinda, sorta`
 const getSomething = () => {
 	// resolve, reject are built into promises
 	return new Promise((resolve, reject) => {
 		// fetch something
-		resolve('yo, this was resolved')
+//		resolve('yo, this was resolved')
 		reject('hey man, what did you do?')
 	});
 }
+
+getSomething().then((data) => {
+	console.log(data)
+}, (err) => {
+	console.log(err)
+})
+
 
