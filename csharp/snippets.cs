6bed08bc294fcea758f17e22b59a37cf6f906836
ausare <ausare@msn.com>
2022-01-04 18:13:55 -0500

    yeah

---
+++ csharp/snippets.cs
@@ -0,0 +1,12 @@
+//This spacing will work
+Console
+.
+WriteLine
+(
+"Hello Word"!
+)
+;
+
+//Example 2
+string firstWorld="Hello";string lastWord="Word";Console.WriteLine(firstWord+" "+lastWord+"!");
+
