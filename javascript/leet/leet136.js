c2829a99fd65b986c9ee46919a750506118f8f53
Jason Gifford <jason.gifford@nielsen.com>
2022-02-16 11:53:30 -0500

    My first successful leet code

---
+++ javascript/leet/leet136.js
@@ -0,0 +1,15 @@
+// Given an array where all numbers repeat besides one, return that one value
+let array = [1]
+console.log(array)
+
+let obj = {}
+for (i of array){
+	if (obj[i]) {
+		obj[i] += 1
+	} else {
+		obj[i] = 1
+	}
+}
+
+let result = Object.keys(obj).find(key => {return obj[key] == 1}) 
+console.log(result) 
