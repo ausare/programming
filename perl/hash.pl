6e8ddcca11e621af06ccf328d75fe8ee34085420
Jason Gifford <jason.gifford@nielsen.com>
2022-05-13 08:33:45 -0400

    added perl hash

--- perl/hash.pl
+++ perl/hash.pl
@@ -4,10 +4,28 @@ my @array = qw ("Copy Edit/cross.o17", "Final Read/cross.017", "Final Read/scope
 
 my $line = @array[1];
 
-
 my $string  = "cross.o17|Copy Edit|Final Read";
 
 my @array = split(/\|/, $string);
 
 my $test = (split(/\./, @array))[1];
 print " here is the test: " . $test;
+
+my @newArray = qw { this is going to be the best thing EVERY 10 };
+print "This is my val => ${val}}";
+
+foreach(@newArray){
+	#print " ${_} \n";
+}
+my %hash = @newArray;
+
+print "\n";
+while(my ($keys, $values) = each %hash){
+	print "${values} \n";
+}
+
+my @val = grep {/g/} @newArray;
+print "\n";
+foreach(@val){
+	print $_;
+}
